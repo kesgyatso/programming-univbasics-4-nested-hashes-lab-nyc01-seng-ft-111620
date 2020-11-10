8e8909f05eba18e056fb7e9a97c73c0c7e988966
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-10 05:32:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,6 +70,12 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+ }
+ 		
+ 		{
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -83,7 +89,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash
 end
 
 def changing_alan
