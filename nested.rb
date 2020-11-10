2503a0327d72833b24220d12522327eccc902b0c
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-10 05:34:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -116,6 +116,7 @@ def changing_alan
 alans_new_info="GUI"
 
 programmer_hash[:alan_kay][:known_for]=alans_new_info
+programmer_hash
 
 end
 
@@ -138,5 +139,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+
 
 end
